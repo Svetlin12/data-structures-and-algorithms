5c8f92b846d43a768a997d2c91eca649dc9ac723
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2019-12-14 17:20:39 +0200

    Update Graph.cpp

--- Graph/Graph, BFS, DFS/Graph.cpp
+++ Graph/Graph, BFS, DFS/Graph.cpp
@@ -1,98 +1,172 @@
 #include <iostream>
-#include <list>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
-typedef vector<list<int>> vli;
-typedef vector<bool> vb;
-
 class Graph
 {
 private:
 
-    vli adj;
-    int v, e;
+	vector<vector<int>> adj;
+	vector<bool> visited;
+	int v, e;
+	vector<vector<pair<int, int>>> G; // first - distance, second - number of node; for use in Djikstra;
 
-    void dfsUtil(vb& visited, int curr)
-    {
-        visited[curr] = true;
-        cout << curr << " ";
-        // add additional checks or return type for different problems
-        for (auto i : adj[curr])
-            if (!visited[i])
-                dfsUtil(visited, i);
-    }
+	void clearVisited()
+	{
+		visited.assign(v + 1, false);
+	}
+
+	void dfsUtil(int current, vector<bool>& visited)
+	{
+		visited[current] = true;
+
+		for (auto child : adj[current])
+			if (!visited[child])
+				dfsUtil(child, visited);
+	}
+
+	bool isCyclicUtil(int curr, int parent)
+	{
+		visited[curr] = true;
+
+		for (auto child : adj[curr])
+		{
+			if (visited[child] && child != parent)
+				return true;
+			else if (!visited[child] && isCyclicUtil(child, curr))
+				return true;
+		}
+
+		return false;
+	}
 
 public:
 
-    Graph(int v = 0) : v(v), e(0)
-    {
-        adj.resize(v + 1);
-    }
+	Graph(int v) : v(v), e(0)
+	{
+		adj.resize(v + 1);
+		visited.resize(v + 1, false);
+		G.resize(v + 1);
+	}
 
-    void dfs(int start)
-    {
-        vb visited(v + 1, 0);
-        dfsUtil(visited, start);
-    }
+	void addEdge(int v1, int v2)
+	{
+		adj[v1].push_back(v2);
+		adj[v2].push_back(v1);
+		e++;
+	}
 
-    void bfs(int start)
-    {
-        vb visited(v + 1, 0);
-        queue<int> q;
-        q.push(start);
-        visited[start] = true;
-        int level = 0;
+	void dfs(int start)
+	{
+		clearVisited();
+		dfsUtil(start, visited);
+	}
 
-        while (!q.empty())
-        {
-            int curr = q.front();
-            q.pop();
-            cout << curr << " ";
+	void bfs(int start)
+	{
+		queue<int> q;
+		q.push(start);
+		clearVisited();
+		visited[start] = true;
 
-            for (auto i : adj[curr])
-            {
-                if (!visited[i])
-                {
-                    visited[i] = true;
-                    q.push(i);
-                }
-            }
+		while (!q.empty())
+		{
+			int curr = q.front();
+			q.pop();
 
-            level++;
-        }
-    }
+			for (auto child : adj[curr])
+			{
+				if (!visited[child])
+				{
+					q.push(child);
+					visited[child] = true;
+				}
+			}
+		}
+	}
 
-    void addEdge(int v1, int v2)
-    {
-        adj[v1].push_back(v2);
-        adj[v2].push_back(v1);
-        e++;
-    }
+	void bfsLevel(int start, int level)
+	{
+		queue<int> q;
+		q.push(start);
+		clearVisited();
+		visited[start] = true;
 
+		int size = 1;
+		int currLevel = 0;
+
+		while (!q.empty())
+		{
+			int curr = q.front();
+			q.pop();
+			size--;
+
+			if (currLevel = level)
+				cout << "visited: " << curr << "\n";
+
+			for (auto child : adj[curr])
+			{
+				if (!visited[child])
+				{
+					q.push(child);
+					visited[child] = true;
+				}
+			}
+
+			if (size == 0)
+			{
+				size = q.size();
+				currLevel++;
+			}
+		}
+	}
+
+	bool isCyclic(int start)
+	{
+		clearVisited();
+		return isCyclicUtil(start, start);
+	}
+
+	void dijkstra(int start)
+	{
+		vector<int> d(G.size(), INT_MAX);
+		priority_queue<pair<int, int>> pq;
+		pq.push({ 0, start });
+
+		while (!pq.empty())
+		{
+			int dist = pq.top().first;
+			int v = pq.top().second;
+			pq.pop();
+
+			d[v] = dist;
+			for (auto child : G[v])
+				pq.push({ dist + child.second, child.first });
+		}
+	}
 };
 
 int main()
 {
-    int v, e, i, u, w;
+	int v, e, i, u, w;
 
-    cin >> v >> e;
+	cin >> v >> e;
 
-    Graph g(v);
+	Graph g(v);
 
-    pair <int, int> p;
-    for (int i = 0; i < e; i++)
-    {
-        cin >> p.first >> p.second;
-        g.addEdge(p.first, p.second);
-    }
+	pair <int, int> p;
+	for (int i = 0; i < e; i++)
+	{
+		cin >> p.first >> p.second;
+		g.addEdge(p.first, p.second);
+	}
 
-    cout << "DFS: ";
-    g.dfs(1);
-    cout << "\nBFS: ";
-    g.bfs(1);
+	cout << "DFS: ";
+	g.dfs(1);
+	cout << "\nBFS: ";
+	g.bfs(1);
 
-    return 0;
+	return 0;
 }
